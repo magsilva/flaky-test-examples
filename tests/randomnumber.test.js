4aee36d551297db117b86dbc30554796bbb06393
Marco Aurélio Graciotto Silva <magsilva@gmail.com>
2024-08-13 16:58:17 -0300

    Reduce the flakiness of the test case to something more reasonable.
    The original chance of occurrence of a failure of the test case of
    random number generation was too high. Thus I changed the thresholds
    considered to evaluate the number.
    
    Another improvement was in the random number generator. The previous
    code would never generate a number in the upper limit. This has been
    fixed. The 'get_random_integer_number' was copied from StackOverflow
    (https://stackoverflow.com/a/29246176).

--- tests/randomnumber.test.js
+++ tests/randomnumber.test.js
@@ -1,11 +1,15 @@
-function get_random_number(a) {
-    return Math.random() * a;
+function get_random_integer_number(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-test('Get random from 0 to 10', () => {
+function get_random_number(a) {
+    return get_random_integer_number(0, a);
+}
+
+test('Get random from 0 to 101', () => {
   const randomNumber = get_random_number(100);
-  const greatherNumber = 1;
-  const lowerNumber = 1;
+  const greatherNumber = 50;
+  const lowerNumber = 10;
 
   expect(randomNumber).toBeLessThan(greatherNumber);
   expect(randomNumber).toBeGreaterThan(lowerNumber);
